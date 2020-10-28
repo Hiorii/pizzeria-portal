419a1c1ba7f032d65bf6e34ad7a9552796b694ad
Bartosz Janiak <janiak.bartosz92@gmail.com>
2020-10-28 10:39:44 +0100

    Add kittchen and login views. Add normalize. Add material ui.

--- src/App.js
+++ src/App.js
@@ -7,20 +7,35 @@ import Waiter from './components/views/Waiter/Waiter';
 import Kitchen from './components/views/Kitchen/Kitchen';
 import Booking from './components/views/Booking/Booking';
 import Dashboard from './components/views/Dashboard/Dashboard';
+import { StylesProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2B4C6F',
+    },
+  },
+});
 
 function App() {
   return (
     <BrowserRouter basename={'/panel'}>
-      <MainLayout>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard} />
-          <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login} />
-          <Route exact path={`${process.env.PUBLIC_URL}/tables/`} component={Tables} />
-          <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/:id`} component={Booking} />
-          <Route exact path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter} />
-          <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen} />
-        </Switch>
-      </MainLayout>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <MainLayout>
+            <Switch>
+              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard} />
+              <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login} />
+              <Route exact path={`${process.env.PUBLIC_URL}/tables/`} component={Tables} />
+              <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/:id`} component={Booking} />
+              <Route exact path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter} />
+              <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen} />
+            </Switch>
+          </MainLayout>
+        </ThemeProvider>
+      </StylesProvider>
     </BrowserRouter>
   );
 }
--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -3,16 +3,22 @@ import PropTypes from 'prop-types';
 import PageNav from '../PageNav/PageNav';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Container from '@material-ui/core/Container';
 
 const MainLayout = ({children}) => {
   return (
     <div>
       <AppBar>
-        <Toolbar>
-          <PageNav />
-        </Toolbar>
+        <Container maxWidth='lg'>
+          <Toolbar disableGutters>
+            <PageNav />
+          </Toolbar>
+        </Container>
       </AppBar>
-      {children}
+      <Container maxWidth='lg'>
+        <Toolbar />
+        {children}
+      </Container>
     </div>
   );
 };
